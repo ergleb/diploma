8bdb4d699c04c23308b0a02100082617acc376f2
Hlib <ergleb23@gmail.com>
2018-04-22 15:13:33 +0300

    A call to find prices

---
+++ src/main/java/com/ergleb/twitterpredictions/scheduling/CurrencyScheduler.java
@@ -0,0 +1,23 @@
+package com.ergleb.twitterpredictions.scheduling;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class CurrencyScheduler {
+
+    @Scheduled (fixedDelay = 5000L)
+    private void getRates() {
+        final String uriPrice = "https://api.coinbase.com/v2/prices/BTC-USD/spot";
+
+        RestTemplate restTemplate = new RestTemplate();
+        String price = restTemplate.getForObject(uriPrice, String.class);
+
+        log.info("price: {}", price);
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(CurrencyScheduler.class);
+}
